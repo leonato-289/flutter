65e919433baeb11bdefa66a986cb9bc60674c30d
aluno <etecae023@hotmail.com>
2025-04-22 11:51:34 -0300

    Salvando altera├º├Áes no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MaterialApp(
     builder: (context, child) {
       return Directionality(textDirection: TextDirection.ltr, child: child!);
     },
-    title: 'GNav',
+    title: 'GNav_Test',
     theme: ThemeData(
       primaryColor: Colors.grey[800],
     ),
